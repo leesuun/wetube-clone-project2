5edf4edcd8f8f86ba0786dd252cb66e5c393580c
leesuun <77526673+leesuun@users.noreply.github.com>
2021-08-29 22:44:56 +0900

    sd

--- build/init.js
+++ build/init.js
@@ -16,8 +16,7 @@ var _app = _interopRequireDefault(require("./app"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-console.log("sad");
-var PORT = process.env.DB_URL || 4002;
+var PORT = process.env.port || process.env.PORT || 4001;
 
 _app["default"].listen(PORT, function () {
   console.log("\u2705Server listening on PORT http://localhost:".concat(PORT));
