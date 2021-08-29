351d62bbd42aa07e94f2ed6abafbdb04f18ecbd4
leesuun <77526673+leesuun@users.noreply.github.com>
2021-08-29 23:46:57 +0900

    port

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ import "./models/User";
 import "./models/Comment";
 import app from "./app";
 
-const PORT = process.env.port || process.env.PORT || 4001;
+const PORT = process.env.PORT || 4001;
 
 app.listen(PORT, () => {
     console.log(`✅Server listening on PORT http://localhost:${PORT}`);
